d74841171f236deba7e3320b26ac9792cbb2460a
Thomas Greening <tomfgreening@gmail.com>
2025-11-06 12:04:15 +0000

    Basic setup and pseudo code notes.

---
+++ server/seed.js
@@ -0,0 +1,1 @@
+// Database seed table SQL queries to go here.
---
+++ server/server.js
@@ -0,0 +1,12 @@
+// Express server functionality and interactiviety with the database will run from here. 
+
+//import packages
+//start or configure packages
+//tell server to use JSON
+//set up a port for the server by listening...
+//set up your database pool
+//create a root route
+
+//=============================================
+//I need a route to READ data from the database
+//I need a route to CREATE new data in the database
